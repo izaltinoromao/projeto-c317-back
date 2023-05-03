c00d426696ee8f165e6fe4de597b17335b33b207
Izaltino Romao Neto <izaltinoromaoneto@hotmail.com>
2023-05-02 21:59:46 -0300

    🎉 init: starting the project

---
+++ src/main/java/br/inatel/projetoc317back/ProjetoC317BackApplication.java
@@ -0,0 +1,13 @@
+package br.inatel.projetoc317back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoC317BackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoC317BackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/inatel/projetoc317back/ProjetoC317BackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.inatel.projetoc317back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoC317BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
