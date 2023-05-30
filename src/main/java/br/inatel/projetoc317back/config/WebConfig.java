bfa897de2efd0cb494516c22be854efea6552c47
Izaltino Romao Neto <izaltinoromaoneto@hotmail.com>
2023-05-30 14:45:18 -0300

    🚧 fix: removing comment

--- src/main/java/br/inatel/projetoc317back/config/WebConfig.java
+++ src/main/java/br/inatel/projetoc317back/config/WebConfig.java
@@ -11,6 +11,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");// <- assim permite de qualquer origem, troque "/**" pelo seu dominio por exemplo "http://meudominio.com"
+        registry.addMapping("/**");
     }
 }
